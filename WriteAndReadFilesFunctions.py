fd39b393647316ae3152c186cfc7a5318578613b
sergnator <86975413+sergnator@users.noreply.github.com>
2023-11-06 14:15:38 +0300

    добавления комментариев

--- WriteAndReadFilesFunctions.py
+++ WriteAndReadFilesFunctions.py
@@ -4,7 +4,7 @@ import sqlite3
 from MainClasses import Planet
 from Constans import DATABASE
 
-
+# запись исключений
 def write_exception(message):
     now = datetime.datetime.now()
     path = f"crusheslogs\\{now.strftime('%c').replace(':', '.').replace(' ', '_')}.txt"
@@ -17,7 +17,7 @@ def write_exception(message):
         with open(path, 'w', encoding='utf-8') as f:
             f.write(message)
 
-
+# функция, которая возращает список экземпляров классов
 def planet_classes():
     con = sqlite3.connect(DATABASE + 'planets.db')
     cur = con.cursor()
@@ -38,7 +38,7 @@ def __convert(obj):
 
     return f"'{obj}'"
 
-
+# добавления планет в базу данных
 def add_obj_in_database(path, data, data_name):
     con = sqlite3.connect(path)
     cur = con.cursor()
